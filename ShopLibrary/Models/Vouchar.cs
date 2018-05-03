ec465b4586acea30fdbabd61d8f17be09756ef56
Ashiful Arefin <arefin.ashiful@gmail.com>
2018-05-03 12:35:36 +0600

    Added Customer Report Print in Customer Form

--- ShopLibrary/Models/Vouchar.cs
+++ ShopLibrary/Models/Vouchar.cs
@@ -7,33 +7,33 @@ using MongoDB.Bson.Serialization.Attributes;
 
 namespace ShopLibrary.Models {
     public class Vouchar {
-        public Memo(Sale sale, Customer customer, Shop shop) : base(sale) {
-            PreviousDue = customer.Debt;
-            ShopName    = shop.ShopName;
-            Address     = shop.Address;
-            ShopContacts = string.Join(", ", shop.ContactNumbers)
-                         + " - "
-                         + string.Join(", ", shop.EmailAddresses);
-        }
+        //public Memo(Sale sale, Customer customer, Shop shop) : base(sale) {
+        //    PreviousDue = customer.Debt;
+        //    ShopName    = shop.ShopName;
+        //    Address     = shop.Address;
+        //    ShopContacts = string.Join(", ", shop.ContactNumbers)
+        //                 + " - "
+        //                 + string.Join(", ", shop.EmailAddresses);
+        //}
 
-        [BsonElement("shopName")]
-        public string ShopName { get; set; }
+        //[BsonElement("shopName")]
+        //public string ShopName { get; set; }
 
-        [BsonElement("shopAddress")]
-        public string Address { get; set; }
+        //[BsonElement("shopAddress")]
+        //public string Address { get; set; }
 
-        [BsonElement("shopContacts")]
-        public string ShopContacts { get; set; }
+        //[BsonElement("shopContacts")]
+        //public string ShopContacts { get; set; }
 
-        [BsonElement("previousDue")]
-        public decimal? PreviousDue { get; set; }
+        //[BsonElement("previousDue")]
+        //public decimal? PreviousDue { get; set; }
 
-        [BsonElement("totalPayable")]
-        public decimal TotalPayable => (PreviousDue == null)
-                                           ? Payable
-                                           : (decimal)PreviousDue + Payable;
+        //[BsonElement("totalPayable")]
+        //public decimal TotalPayable => (PreviousDue == null)
+        //                                   ? Payable
+        //                                   : (decimal)PreviousDue + Payable;
 
-        [BsonElement("currentDue")]
-        public decimal CurrentDue => TotalPayable - Paid;
+        //[BsonElement("currentDue")]
+        //public decimal CurrentDue => TotalPayable - Paid;
     }
 }
